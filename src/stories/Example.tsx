08c204f54969c25453119c1e790feee8736b7cff
Filip Chalupa <chalupa.filip@gmail.com>
2023-03-27 13:52:35 +0200

    Example: add link to store

--- src/stories/Example.tsx
+++ src/stories/Example.tsx
@@ -1,9 +1,34 @@
-import type { FunctionComponent } from 'react'
+import { FunctionComponent, useMemo } from 'react'
 import { usePlatform } from '../usePlatform'
 import './global.css'
 
 export const Example: FunctionComponent = () => {
 	const platform = usePlatform()
+
+	const store = useMemo<null | {
+		name: string
+		link: string
+	}>(() => {
+		if (platform === 'windows') {
+			return {
+				name: 'Microsoft Store',
+				link: 'https://apps.microsoft.com/',
+			}
+		}
+		if (platform === 'android') {
+			return {
+				name: 'Google Play',
+				link: 'https://play.google.com/',
+			}
+		}
+		if (platform === 'ios') {
+			return {
+				name: 'Apple App Store',
+				link: 'https://apps.apple.com/',
+			}
+		}
+		return null
+	}, [platform])
 
 	return (
 		<>
@@ -13,6 +38,11 @@ export const Example: FunctionComponent = () => {
 			<p>
 				Your platform is <output>{platform}</output>.
 			</p>
+			{store && (
+				<p>
+					Your platform specific store is <a href={store.link}>{store.name}</a>.
+				</p>
+			)}
 		</>
 	)
 }
